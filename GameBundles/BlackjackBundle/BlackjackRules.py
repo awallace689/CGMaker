ae61f45daa23dfadce56f5db252dcbe207a37c75
Adam Wallace <awallace689@gmail.com>
2018-10-19 16:25:28 -0600

    Menu.update and methods that call it now take param: up_list type 'list'

--- GameBundles/BlackjackBundle/BlackjackRules.py
+++ GameBundles/BlackjackBundle/BlackjackRules.py
@@ -1,4 +1,5 @@
 from Templates.ABCRules import ABCRules, Phase
+from Templates.Player import Player
 from GameBundles.BlackjackBundle.BlackjackTemplateMods import BlackjackDeck, BlackjackCard
 
 
@@ -14,21 +15,32 @@ class BlackjackRules(ABCRules):
 
 
 class BettingPhase(Phase):
-    _methods = []
-
     @property
     def methods(self):
         return self._methods
+
+    # returns list of tuples "('function obj', 'info string')"
+    _methods = []
+
+    def bet(self, player: Player, amount):
+        player.take_bank(amount)
 
 
 class Menu:
     def __init__(self):
         self._menu = "_UNASSIGNED_\n"
 
-    def update(self, up_dict):
+    @property
+    def get_str(self):
+        return self._menu
+
+    def update(self, up_list):
         print_string = ""
-        for key, val in up_dict.items():
-            print_string += "> {}) {}: {}\n".format(int(key) + 1, val.__name__(), str(key))
+        option_num = 1
+
+        for i in range(up_list):
+            print_string += "> {}) {}: {}\n".format(i + 1, str(up_list[i][0]), up_list[i][1])
+            option_num += 1
 
         if print_string != "":
             self._menu = print_string
@@ -39,18 +51,19 @@ class Menu:
         return self._menu
 
     def generate(self, up_dict):
+        self._menu = ""
         self._menu = self.update(up_dict)
         return self._menu
 
 
-def get_input(menu=None, **kwargs):
+def get_input(up_list, info_list, menu=None):
     if menu is None:
         uin = input("> ")
 
     elif isinstance(menu, Menu):
         try:
-            uin = int(input(menu.generate(kwargs)))
-            assert uin <= kwargs.__len__()
+            uin = int(input(menu.generate(up_list, info_list)))
+            assert uin <= up_list.__len__()
             assert uin >= 1
 
         except ValueError:
@@ -58,15 +71,14 @@ def get_input(menu=None, **kwargs):
             return get_input(menu)
 
         except AssertionError:
-            print("*Selection out of range. Try again...")
+            print("*Selection out of range. Try again...*")
             return get_input(menu)
 
     else:
-        raise TypeError("'menu' is type: '{}', expected type: 'Menu'.".format(type(menu)))
+        raise TypeError("'menu' is type: '{}', expected object type: 'Menu'.".format(type(menu)))
 
     return uin
 
 
 m = Menu()
-inp = get_input(m)
-print(inp)
+inp = get_input(BettingPhase.methods, m)
